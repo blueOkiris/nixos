5010f5097aefcc68f5287f45a84b98ca26a3fa90
Dylan Turner <dylantdmt@gmail.com>
2022-11-20 23:19:26 -0600

    Alterations for messed up nvidia settings

--- .config/i3/arandr.sh
+++ .config/i3/arandr.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-MONITOR_FOUND=`xrandr --listmonitors | grep -i DP-0`
+xrandr --auto
+xrandr --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0
+feh --bg-fill $HOME/Pictures/Wallpapers/botw1.jpg
 
+MONITOR_FOUND=`xrandr --listmonitors | grep -i DP-0`
 if [ "$MONITOR_FOUND" != "" ]; then
     xrandr --output DP-0 --primary --mode 3440x1440 --pos 0x0 --output eDP-1-1 --mode 1920x1080 --pos 3440x0
     sleep 2
@@ -9,4 +12,6 @@ if [ "$MONITOR_FOUND" != "" ]; then
     sleep 2
     feh --bg-fill $HOME/Pictures/Wallpapers/botw-widexcf.png $HOME/Pictures/Wallpapers/botw1.jpg
 fi
+
+source $HOME/.config/polybar/launch.sh
 
