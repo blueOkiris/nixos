106196be7858b3105dacb6e8700192a2e71c56e2
Dylan Turner <dylan.turner@tutanota.com>
2023-06-21 19:57:40 -0500

    Xresources: moved to nix
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- home-configuration.nix
+++ home-configuration.nix
@@ -6,6 +6,17 @@
     imports = [ <home-manager/nixos> ];
     home-manager.useGlobalPkgs = true;
     home-manager.users.dylan = { pkgs, ... }: {
+        home.stateVersion = "23.05";
+        home.packages = [ ];
+        xresources.properties = {
+            "Xft.antialias" = true;
+            "Xft.hinting" = true;
+            "Xft.rgba" = "rgba";
+            "Xft.hintstyle" = "hintslight";
+            "Xft.dpi" = 96;
+            "Xcursor.size" = 24;
+            "Xcursor.theme" = "Breeze_Default";
+        };
         programs.zsh = {
             enable = true;
             enableAutosuggestions = true;
@@ -23,38 +34,38 @@
                 "\$(date +%Y)" = "echo \"YEAR OF THE LINUX DESKTOP!\"";
             };
             initExtra = "
-# Theme - Based on oh-my-zsh 'gentoo' theme
-autoload -Uz colors && colors
-autoload -Uz vcs_info
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr '%F{red}*'   # display this when there are unstaged changes
-zstyle ':vcs_info:*' stagedstr '%F{yellow}+'  # display this when there are staged changes
-zstyle ':vcs_info:*' actionformats '%F{5}(%F{2}%b%F{3}|%F{1}%a%c%u%m%F{5})%f '
-zstyle ':vcs_info:*' formats '%F{5}(%F{2}%b%c%u%m%F{5})%f '
-zstyle ':vcs_info:svn:*' branchformat '%b'
-zstyle ':vcs_info:svn:*' actionformats '%F{5}(%F{2}%b%F{1}:%{3}%i%F{3}|%F{1}%a%c%u%m%F{5})%f '
-zstyle ':vcs_info:svn:*' formats '%F{5}(%F{2}%b%F{1}:%F{3}%i%c%u%m%F{5})%f '
-zstyle ':vcs_info:*' enable git cvs svn
-zstyle ':vcs_info:git*+set-message:*' hooks untracked-git
-+vi-untracked-git() {
-  if command git status --porcelain 2>/dev/null | command grep -q '??'; then
-    hook_com[misc]='%F{red}?'
-  else
-    hook_com[misc]=''
-  fi
-}
-gentoo_precmd() {
-  vcs_info
-}
-setopt prompt_subst
-autoload -U add-zsh-hook
-add-zsh-hook precmd gentoo_precmd
-PROMPT='%(!.%B%F{red}.%B%F{green}%n@)%m %F{blue}%(!.%1~.%~) \${vcs_info_msg_0_}%F{blue}%(!.#.$)%k%b%f '
+                # Theme - Based on oh-my-zsh 'gentoo' theme
+                autoload -Uz colors && colors
+                autoload -Uz vcs_info
+                zstyle ':vcs_info:*' check-for-changes true
+                zstyle ':vcs_info:*' unstagedstr '%F{red}*'   # display this when there are unstaged changes
+                zstyle ':vcs_info:*' stagedstr '%F{yellow}+'  # display this when there are staged changes
+                zstyle ':vcs_info:*' actionformats '%F{5}(%F{2}%b%F{3}|%F{1}%a%c%u%m%F{5})%f '
+                zstyle ':vcs_info:*' formats '%F{5}(%F{2}%b%c%u%m%F{5})%f '
+                zstyle ':vcs_info:svn:*' branchformat '%b'
+                zstyle ':vcs_info:svn:*' actionformats '%F{5}(%F{2}%b%F{1}:%{3}%i%F{3}|%F{1}%a%c%u%m%F{5})%f '
+                zstyle ':vcs_info:svn:*' formats '%F{5}(%F{2}%b%F{1}:%F{3}%i%c%u%m%F{5})%f '
+                zstyle ':vcs_info:*' enable git cvs svn
+                zstyle ':vcs_info:git*+set-message:*' hooks untracked-git
+                +vi-untracked-git() {
+                  if command git status --porcelain 2>/dev/null | command grep -q '??'; then
+                    hook_com[misc]='%F{red}?'
+                  else
+                    hook_com[misc]=''
+                  fi
+                }
+                gentoo_precmd() {
+                  vcs_info
+                }
+                setopt prompt_subst
+                autoload -U add-zsh-hook
+                add-zsh-hook precmd gentoo_precmd
+                PROMPT='%(!.%B%F{red}.%B%F{green}%n@)%m %F{blue}%(!.%1~.%~) \${vcs_info_msg_0_}%F{blue}%(!.#.$)%k%b%f '
 
-# Extra config options
-setopt extendedglob
-bindkey \"^[[3~\" delete-char
-zstyle ':completion:*' menu select
+                # Extra config options
+                setopt extendedglob
+                bindkey \"^[[3~\" delete-char
+                zstyle ':completion:*' menu select
             ";
             localVariables = {
                 TERM = "xterm-256color";
@@ -62,7 +73,5 @@ zstyle ':completion:*' menu select
                 EDITOR = "nvim";
             };
         };
-        home.stateVersion = "23.05";
-        home.packages = [ ];
     };
 }
