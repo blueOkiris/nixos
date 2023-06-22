52b44ce43269af402b15627a8b88ce5e6ab12782
Dylan Turner <dylan.turner@tutanota.com>
2023-06-21 20:54:46 -0500

    dunst: move to nix
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- home-configuration.nix
+++ home-configuration.nix
@@ -88,6 +88,23 @@
                 };
             };
         };
+        services.dunst = {
+            enable = true;
+            settings = {
+                global = {
+                    frame_width = 5;
+                    icon_theme = "Papirus-Dark";
+                };
+                urgency_low = {
+                    background = "#282a36";
+                    foreground = "#f8f8f2";
+                };
+                urgency_normal = {
+                    background = "#282a36";
+                    foreground = "#f8f8f2";
+                };
+            };
+        };
         gtk = {
             enable = true;
             theme.name = "Arc-Dark";
