70070c404c1bd46c4858f11f98b73fbd9433c4c1
Dylan Turner <dylan.turner@tutanota.com>
2023-07-08 19:53:40 -0500

    hardware-configuration: enable wireless xbox
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -57,6 +57,9 @@
         "\nSUBSYSTEM==\"usb\", ENV{DEVTYPE}==\"usb_device\", ATTRS{idVendor}==\"057e\", "
             + "ATTRS{idProduct}==\"0337\", MODE=\"0666\"";
 
+    # Enable wireless XBox controller
+    hardware.xpadneo.enable = true;
+
     # Bluetooth enable
     hardware.bluetooth.enable = true;
 
