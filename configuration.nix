f1c4e1e39c002831f385e409212f924bc32f68dc
Dylan Turner <dylan.turner@tutanota.com>
2023-08-16 15:10:46 -0500

    Update to Slippi 3.2.2
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- configuration.nix
+++ configuration.nix
@@ -54,8 +54,8 @@ let
             app_name = "Slippi_Online-x86_64.AppImage";
             gh_proj = "Ishiiruka";
             gh_user = "project-slippi";
-            version = "3.2.0";
-            hash = "1y0xkih9kq4id0lzs666pjvdqrbkx72smqkrnz3j0pwxm239aj9b";
+            version = "3.2.2";
+            hash = "12k344ky4kp4i9sr847sh9hzq1h8w42msd25gkf5zpmx0s7v8y4r";
         in pkgs.appimageTools.wrapType2 {
             name = "slippi";
             extraPkgs = pkgs: [
