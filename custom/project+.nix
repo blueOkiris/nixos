cf78770be881c055257333f632944380a8f0d135
Dylan Turner <dylan.turner@tutanota.com>
2023-12-09 19:10:44 -0600

    Fix project+ user link
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- custom/project+.nix
+++ custom/project+.nix
@@ -7,7 +7,7 @@ let
         let
             app_name = "Faster_Project_Plus-x86-64.AppImage";
             gh_proj = "FPM-AppImage";
-            gh_user = "Ishiiruka";
+            gh_user = "jlambert360";
             version = "2.4.2";
             hash = "0vgg9xvlk94mp0ip5473py381l2bpd2anqibpyp0jkp4xq6wdkm3";
         in pkgs.appimageTools.wrapType2 {
@@ -16,6 +16,8 @@ let
                 pkgs.gmp
                 pkgs.mpg123
                 pkgs.libmpg123
+                pkgs.webkitgtk
+                pkgs.libthai
             ];
             src = builtins.fetchurl {
                 url =
