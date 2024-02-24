51961bb3e9b741ca8cd054430d65e7e93e7c998d
Dylan Turner <dylan.turner@tutanota.com>
2024-02-23 21:26:37 -0600

    Disabled gnome portal
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- configuration.nix
+++ configuration.nix
@@ -233,9 +233,10 @@ in {
     services.dbus.enable = true;
     xdg.portal = {
         enable = true;
+        wlr.enable = true;
         extraPortals = [
             #pkgs.xdg-desktop-portal-gtk
-            pkgs.xdg-desktop-portal-gnome
+            #pkgs.xdg-desktop-portal-gnome
             #pkgs.xdg-desktop-portal-hyprland
         ];
     };
