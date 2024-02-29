26370eb15bbee46e105ce264b147cb45ec2b3afb
Dylan Turner <dylan.turner@tutanota.com>
2024-02-29 09:09:43 -0600

    tutanota: downgrade one vers
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- custom/tutanota-appimage.nix
+++ custom/tutanota-appimage.nix
@@ -8,8 +8,8 @@ let
             app_name = "tutanota-desktop-linux";
             gh_user = "tutao";
             gh_proj = "tutanota";
-            version = "218.240227.0";
-            hash = "1s66lkywwvc6afmqjfjyq5zyz8r06jww8zicnfp6lrzi5p87z2kp";
+            version = "218.240219.0";
+            hash = "1rngmb8pmz6vq0b5psfva5ld7gm3d0ibdxrzhw0f0rxgb9bqdh5h";
         in pkgs.appimageTools.wrapType2 {
             name = "tutanota";
             extraPkgs = pkgs: [
