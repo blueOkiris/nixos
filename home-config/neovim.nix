fc10bae96437243adeb25b02eb83fd9c5d710125
Dylan Turner <dylan.turner@tutanota.com>
2024-03-19 00:40:38 -0500

    neovim: enable ale for Godot
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- home-config/neovim.nix
+++ home-config/neovim.nix
@@ -55,9 +55,14 @@ in {
                     \\ 'cpp': [ 'clang' ],
                     \\ 'c': [ 'clang' ],
                     \\ 'haskell': [ 'hlint' ],
-                    \\ 'cs': [ 'OmniSharp' ],
-                    \\ 'gdscript': [ 'godot' ]
+                    \\ 'cs': [ 'OmniSharp' ]
                 \\}
+                call ale#linter#Define('gdscript', {
+                    \\ 'name': 'godot',
+                    \\ 'lsp': 'socket',
+                    \\ 'address': '127.0.0.1:6008',
+                    \\ 'project_root': 'project.godot',
+                \\})
 
                 set splitright
                 set splitbelow
